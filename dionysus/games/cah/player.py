810681f95c0371ad4fa0791bdad91e4faeb3a2e6
Ethan Harstad <ethanharstad@gmail.com>
2021-06-09 04:28:20 +0000

    What have I done

--- dionysus/games/cah/player.py
+++ dionysus/games/cah/player.py
@@ -2,6 +2,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class Player:
     id: int
     name: str
@@ -13,17 +14,17 @@ class Player:
         self.name = name
         self.score = 0
         self.hand = []
-    
+
     def __eq__(self, other):
-        if (isinstance(other, Player)):
+        if isinstance(other, Player):
             return self.id == other.id
         return False
-    
+
     def __hash__(self):
         return self.id
-    
+
     def __str__(self):
         return self.name
-    
+
     def __repr__(self):
         return "Player({0.id}, {0.name})".format(self)
